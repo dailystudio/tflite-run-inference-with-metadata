62ec4617a1de198f3c9f1b70012a28e9b3a87b40
Nan YE <dailystudio2010@gmail.com>
2021-02-28 22:06:37 +0800

    [tflite-task-api][DEV]: set inference device to CPU permanently; [tflite-task-api][DEV]: add test images and results;

--- tflite-task-api/app/src/main/java/com/dailystudio/tflite/example/taskapi/ViewerApplication.kt
+++ tflite-task-api/app/src/main/java/com/dailystudio/tflite/example/taskapi/ViewerApplication.kt
@@ -1,8 +1,16 @@
 package com.dailystudio.tflite.example.taskapi
 
 import com.dailystudio.tensorflow.lite.viewer.BaseTFLiteModelViewApplication
+import com.dailystudio.tensorflow.lite.viewer.ui.InferenceSettingsPrefs
+import org.tensorflow.lite.support.model.Model
 
 class ViewerApplication : BaseTFLiteModelViewApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        InferenceSettingsPrefs.instance.device = Model.Device.CPU.toString()
+    }
 
     override fun isDebugBuild(): Boolean {
         return BuildConfig.DEBUG
